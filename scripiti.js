8084dc0d7ec7db05a8130e8e53101a7bc95e6924
ChRxT09 <86499952+ChRxT09@users.noreply.github.com>
2022-04-30 15:40:48 -0300

    Add files via upload

---
+++ scripiti.js
@@ -0,0 +1,12 @@
+new mojs.Shape({
+    shape:        'circle',
+    scale:         { 0 : 1 },
+    fill:          '#12FA3B',
+    duration:      1000,
+    delay:         1000,
+    easing:        'cubic.out',
+    repeat:        2,
+    isYoyo:         true,
+    easing:       'elastic.inout',
+    repeat:        1,
+  }).play();
